ff68a6dda476eade2aad749360bfd2146505df03
danika <theamdanik@cy-tech.fr>
2026-05-27 16:58:26 +0200

    Ajout BDD et Image

--- src/BaseDeDonnees.java
+++ src/BaseDeDonnees.java
@@ -1,6 +1,35 @@
+import org.ejml.simple.SimpleMatrix;
+import javafx.scene.image.Image;
+import java.io.file;
 
 public class BaseDeDonnees {
 	
-	private String cheminRacine;
+	private String chemin; // Chemin des images d'apprentissage
+	private Map<String,Image> images; // Map -> clé = nom, valeur = photo
+	private SimpleMatrix matricesImages; // Matrice total
+
+	public BaseDeDonnees(){
+		this.chemin = "donnees/apprentissage/";
+        this.images
+	}
+
+	public int getApprentissage(){
+		File[] f = monRepertoire.listFiles();
+		int x = 0;
+		for (int i = 0 ; i < f.length ; i++) {
+		  if (f[i].isFile()) {
+			x++;
+		  }
+		}
+
+		return x;
+	}
+
+	public static void main(String[] args){
+
+	BaseDeDonnees bdd = new BaseDeDonnees();
+
+		System.out.println(bdd.getApprentissage());
+	}
 	
 }
---
+++ src/ImageVect.java
@@ -0,0 +1,70 @@
+import org.ejml.simple.SimpleMatrix;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+
+public class ImageVect {
+
+    private int longueur;
+    private int largeur;
+    private String nom; // nom du fichier
+    private SimpleMatrix vecteurCol;
+    private BufferedImage image;
+    private File fichier;
+
+    public ImageVect(String chemin) throws IOException{
+        this.fichier = new File(chemin);
+        this.image = ImageIO.read(this.fichier);
+
+        if (this.image == null){
+            throw new IOException("Le chemin ne marche pas!");
+        }
+
+        this.nom = this.fichier.getName();
+        this.nom = this.nom.substring(0, this.nom.length() - 4);
+        this.longueur = this.image.getHeight();
+        this.largeur = this.image.getWidth();
+        this.vecteurCol = new SimpleMatrix(1,this.largeur*this.longueur);
+    }
+
+    public String getNom(){
+        return this.nom;
+    }
+
+    public int getLongueur(){
+        return this.longueur;
+    }
+
+    public int getLargeur(){
+        return this.largeur;
+    }
+
+    public void vectoriser(){
+        int index = 0;
+        for (int i=0;i<this.getLongueur();i++){
+            for (int j=0;j<this.getLargeur();j++){
+                
+            }
+        }
+    }
+
+    public static void main(String[] args){
+
+        try {
+            ImageVect img = new ImageVect("donnees/apprentissage/Alysa Liu/alysa1.jpg");
+            System.out.println("Image chargée avec succès !");
+            System.out.println("Nom du fichier : " + img.getNom());
+            System.out.println("Dimensions : " + img.getLargeur() + " x " + img.getLongueur() + " pixels");
+            System.out.println(img.vecteurCol);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        
+
+
+    }
+
+
+
+}
