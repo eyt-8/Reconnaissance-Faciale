093cbb3f28da2f2fb7d2938aa87ee32d910bc904
Nylan01 <nylanpaillassa01@gmail.com>
2026-05-29 12:09:39 +0200

    pas grand chose

--- src/App.java
+++ src/App.java
@@ -6,27 +6,17 @@ import org.ejml.simple.SimpleMatrix;
 
 public class App {
 
-    // // 4. Eigenfaces
-    //     Eigenfaces eigenfaces = new Eigenfaces();
-    //     // Attention au format retourné par svd.getbValSinguliere() : Eigenfaces.construire() s'attend 
-    //     // à recevoir un vecteur colonne pour les valeurs propres, et non une matrice diagonale
-    //     eigenfaces.construire(svd.getbValSinguliere(), svd.getVectPropATA()); 
-    //     eigenfaces.setVisageMoyen(acp.getVisageMoyen()); // Setter à ajouter dans Eigenfaces
-    //     eigenfaces.selectionnerK(0.95); // On garde 95% de la variance
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
+        // Chargement des images : matrice => vecteurs => matrices des images (Danika)
+        // Requiert BDD et ImageVect
         BaseDeDonnees bdd = new BaseDeDonnees();
         bdd.associerIdNom();
         List<ImageVect> images = new ArrayList<>();
         images = bdd.getReferences();
         
         SimpleMatrix matriceComplete = bdd.matriceTot(images);
-        
-
-
-        // Chargement des images : matrice => vecteurs => matrices des images (Danika)
-        // Requiert BDD et ImageVect
+    
         
         // Calculer le visage moyen => centrer la matrice image (Virgile)
         // Requiert ACP
@@ -34,6 +24,12 @@ public class App {
         // Calcul de la matrice de variance / covariance + réduction des dimensions (Remplaçants de Soraya => Maël et Nylan)
         // Requiert SVD (Maël) et Propre (Nylan) => Eigenfaces (Nylan)
         
+    //     Eigenfaces eigenfaces = new Eigenfaces();
+    //     // Attention au format retourné par svd.getbValSinguliere() : Eigenfaces.construire() s'attend 
+    //     // à recevoir un vecteur colonne pour les valeurs propres, et non une matrice diagonale
+    //     eigenfaces.construire(svd.getbValSinguliere(), svd.getVectPropATA()); 
+    //     eigenfaces.setVisageMoyen(acp.getVisageMoyen()); // Setter à ajouter dans Eigenfaces
+    //     eigenfaces.selectionnerK(0.95); // On garde 95% de la variance
         // Prendre image qui sera reconnue : Danika
         // Requiert : BaseDeDonnees
         
