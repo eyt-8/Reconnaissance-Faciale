bbeb769fb0510b9880cf3e70b4ca3b0908934b4f
Virgile <caumontvir@cy-tech.fr>
2026-05-29 10:29:17 +0200

    Acp testée et corrigée

--- src/Acp.java
+++ src/Acp.java
@@ -15,7 +15,7 @@ public class Acp {
 	 * Sur ejml un vecteur est défini par SimpleMatrix avec seulement une colonne
 	 * */
 	private SimpleMatrix visage_moyen;
-	
+
 	/** 
 	 * Matrice {@link BaseDeDonnees#matriceImages} centrée
 	 * */
@@ -27,48 +27,89 @@ public class Acp {
 	
 	public Acp(BaseDeDonnees donnees) {
 		this.donnees=donnees;
-		this.calcVisageMoyen();
-		this.centrer();
+		this.calcVisageMoyen(this.donnees.getMatriceImages());
+		this.centrer(this.donnees.getMatriceImages());
+	}
+	
+	/** 
+	 * Constructeur utilisant directement une matrice
+	 * Utilisée principalement pour réaliser des tests*/
+	public Acp(SimpleMatrix test) {
+		this.donnees=null;
+		this.calcVisageMoyen(test);
+		this.centrer(test);
+	}
+	
+	/** 
+	 * Getters / Setters */
+	public SimpleMatrix getVisage_moyen() {
+		return visage_moyen;
+	}
+
+	public void setVisage_moyen(SimpleMatrix visage_moyen) {
+		this.visage_moyen = visage_moyen;
+	}
+
+	public SimpleMatrix getMatrice_centree() {
+		return matrice_centree;
+	}
+
+	public void setMatrice_centree(SimpleMatrix matrice_centree) {
+		this.matrice_centree = matrice_centree;
+	}
+
+	public BaseDeDonnees getDonnees() {
+		return donnees;
+	}
+
+	public void setDonnees(BaseDeDonnees donnees) {
+		this.donnees = donnees;
 	}
 	
 	/** 
 	 * Permet de calculer le visage moyen à partir de la base de données
-	 * Modifie {@link Acp#visage_moyen} 
-	 * Utilise {@link BaseDeDonnees#matriceImage}
+	 * Modifie {@link Acp#visage_moyen}
+	 * Utilise {@link BaseDeDonnees#matriceImage} ou une matrice de test
 	 * */
-	public void calcVisageMoyen() {
+	public void calcVisageMoyen(SimpleMatrix visages) {
 		// Le nombre d'images correspond au nombre de lignes
-		int taille = this.donnees.matriceImages.getNumCols();
+		int taille = visages.getNumCols();
 		// Le nombre de colonnes correspond au nombre de colonnes
-		int taille_pixels = this.donnees.matriceImages.getNumRows();
+		int taille_pixels = visages.getNumRows();
+		
+		this.visage_moyen = new SimpleMatrix(taille_pixels,1);
 		
 		// On calcule la moyenne sur chaque ligne et on l'ajoute au vecteur du visage moyen
 		
-		for (int i=0;i<taille;i++) {
-			double somme = 0;
-			for (int j=0;j<taille_pixels;j++) {
-				// j,i car on fixe la ligne et on se déplace sur la colonne
-				somme = somme + this.donnees.matriceImages.get(j,i);
+		double somme;
+		
+		for (int i=0;i<taille_pixels;i++) {
+			somme = 0;
+			for (int j=0;j<taille;j++) {
+				somme = somme + visages.get(i,j);
 			}
 			this.visage_moyen.set(i,0,somme/taille);
 		}
 	}
 	
 	/** 
-	 * Centre la matrice {@link BaseDeDonnees#matriceImages}
+	 * Centre la matrice {@link BaseDeDonnees#matriceImages} ou une matrice de test
 	 * Modifie {@link Acp#matrice_centree}
 	 * */
-	public void centrer() {
+	public void centrer(SimpleMatrix visages) {
 		// Le nombre d'images correspond au nombre de lignes
-		int taille = this.donnees.matriceImages.getNumCols();
+		int taille = visages.getNumCols();
 		// Le nombre de colonnes correspond au nombre de colonnes
-		int taille_pixels = this.donnees.matriceImages.getNumRows();
+		int taille_pixels = visages.getNumRows();
+		
+		this.matrice_centree = new SimpleMatrix(taille_pixels,taille);
+		
 		double elt = 0;
-		for (int i=0;i<taille;i++) {
+		for (int i=0;i<taille_pixels;i++) {
 			elt=0;
-			for (int j=0;j<taille_pixels;j++) {
+			for (int j=0;j<taille;j++) {
 				// On récupère l'élément de coordonnées (i,j) de la matrice
-				elt = this.donnees.matriceImages.get(i,j);
+				elt = visages.get(i,j);
 				
 				// On le soustrait à la moyenne de la ligne
 				elt = elt-this.visage_moyen.get(i,0);
@@ -103,6 +144,12 @@ public class Acp {
 			System.out.println(" )");
 		}
 		
-		// La suite requiert BaseDeDonnees
+		System.out.println(a.toString());
+		
+		Acp acp = new Acp(a);
+		// Affichage du visage moyen
+		System.out.println(acp.getVisage_moyen().toString());
+		// Affichage de la matrice centrée
+		System.out.println(acp.getMatrice_centree());
 	}
 }
