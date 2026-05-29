da94c341c377ae7f46f5c7cb47a8b7c22c25626d
Virgile <caumontvir@cy-tech.fr>
2026-05-29 11:14:50 +0200

    Ajout des actions à suivree

--- src/App.java
+++ src/App.java
@@ -1,6 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        // Test
+        
+        // Chargement des images : matrice => vecteurs => matrices des images (Danika)
+        // Requiert BDD et ImageVect
+        
+        // Calculer le visage moyen => centrer la matrice image (Virgile)
+        // Requiert ACP
+        
+        // Calcul de la matrice de variance / covariance + réduction des dimensions (Remplaçants de Soraya => Maël et Nylan)
+        // Requiert SVD (Maël) et Propre (Nylan) => Eigenfaces (Nylan)
+        
+        // Prendre image qui sera reconnue : Danika
+        // Requiert : BaseDeDonnees
+        
+        // Reconnaître l'image : Remplaçants de Soraya => Virgile
+        // Requiert : Maël (Projection) et Reconnaissance (Virgile)
+        
+        // Calculer l'erreur (Virgile)
+        // Requiert : Comparaison (Virgile)
     }
 }
