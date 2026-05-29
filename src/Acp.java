fb4997e2359be0210dcdfb83eb0464382c46b66c
Virgile <caumontvir@cy-tech.fr>
2026-05-29 12:03:20 +0200

    Tabulation appliquée pour les commentaires javadoc

--- src/Acp.java
+++ src/Acp.java
@@ -1,48 +1,49 @@
 
-
-
 import org.ejml.simple.SimpleMatrix;
 
 /**
  * Classe mettant en place l'ACP
+ * 
  * @author virgile
  * @version 0.1
- * */
+ */
 public class Acp {
-	/** 
-	 * Représente le visage moyen, un vecteur
-	 * Sur ejml un vecteur est défini par SimpleMatrix avec seulement une colonne
-	 * */
+	/**
+	 * Représente le visage moyen, un vecteur Sur ejml un vecteur est défini par
+	 * SimpleMatrix avec seulement une colonne
+	 */
 	private SimpleMatrix visage_moyen;
 
-	/** 
+	/**
 	 * Matrice {@link BaseDeDonnees#matriceImages} centrée
-	 * */
+	 */
 	private SimpleMatrix matrice_centree;
-	/** 
+	/**
 	 * Voir {@link BaseDeDonnees}
-	 * */
+	 */
 	private BaseDeDonnees donnees;
-	
+
 	public Acp(BaseDeDonnees donnees) {
-		this.donnees=donnees;
+		this.donnees = donnees;
 		this.calcVisageMoyen(this.donnees.getMatriceImages());
 		this.centrer(this.donnees.getMatriceImages());
 	}
-	
-	/** 
-	 * Constructeur utilisant directement une matrice
-	 * Utilisée principalement pour réaliser des tests
+
+	/**
+	 * Constructeur utilisant directement une matrice Utilisée principalement pour
+	 * réaliser des tests
+	 * 
 	 * @param test matrice de test
-	 * */
+	 */
 	public Acp(SimpleMatrix test) {
-		this.donnees=null;
+		this.donnees = null;
 		this.calcVisageMoyen(test);
 		this.centrer(test);
 	}
-	
-	/** 
-	 * Getters / Setters */
+
+	/**
+	 * Getters / Setters
+	 */
 	public SimpleMatrix getVisage_moyen() {
 		return visage_moyen;
 	}
@@ -66,88 +67,91 @@ public class Acp {
 	public void setDonnees(BaseDeDonnees donnees) {
 		this.donnees = donnees;
 	}
-	
-	/** 
-	 * Permet de calculer le visage moyen à partir de la base de données
-	 * Modifie {@link Acp#visage_moyen}
-	 * @param visages Utilise {@link BaseDeDonnees#matriceImage} ou une matrice de test
-	 * */
+
+	/**
+	 * Permet de calculer le visage moyen à partir de la base de données Modifie
+	 * {@link Acp#visage_moyen}
+	 * 
+	 * @param visages Utilise {@link BaseDeDonnees#matriceImage} ou une matrice de
+	 *                test
+	 */
 	public void calcVisageMoyen(SimpleMatrix visages) {
 		// Le nombre d'images correspond au nombre de lignes
 		int taille = visages.getNumCols();
 		// Le nombre de colonnes correspond au nombre de colonnes
 		int taille_pixels = visages.getNumRows();
-		
-		this.visage_moyen = new SimpleMatrix(taille_pixels,1);
-		
-		// On calcule la moyenne sur chaque ligne et on l'ajoute au vecteur du visage moyen
-		
+
+		this.visage_moyen = new SimpleMatrix(taille_pixels, 1);
+
+		// On calcule la moyenne sur chaque ligne et on l'ajoute au vecteur du visage
+		// moyen
+
 		double somme;
-		
-		for (int i=0;i<taille_pixels;i++) {
+
+		for (int i = 0; i < taille_pixels; i++) {
 			somme = 0;
-			for (int j=0;j<taille;j++) {
-				somme = somme + visages.get(i,j);
+			for (int j = 0; j < taille; j++) {
+				somme = somme + visages.get(i, j);
 			}
-			this.visage_moyen.set(i,0,somme/taille);
+			this.visage_moyen.set(i, 0, somme / taille);
 		}
 	}
-	
-	/** 
+
+	/**
 	 * Centre la matrice {@link BaseDeDonnees#matriceImages} ou une matrice de test
 	 * Modifie {@link Acp#matrice_centree}
+	 * 
 	 * @param visages {@link BaseDeDonnees#matriceImage} ou une matrice de test
-	 * */
+	 */
 	public void centrer(SimpleMatrix visages) {
 		// Le nombre d'images correspond au nombre de lignes
 		int taille = visages.getNumCols();
 		// Le nombre de colonnes correspond au nombre de colonnes
 		int taille_pixels = visages.getNumRows();
-		
-		this.matrice_centree = new SimpleMatrix(taille_pixels,taille);
-		
+
+		this.matrice_centree = new SimpleMatrix(taille_pixels, taille);
+
 		double elt = 0;
-		for (int i=0;i<taille_pixels;i++) {
-			elt=0;
-			for (int j=0;j<taille;j++) {
+		for (int i = 0; i < taille_pixels; i++) {
+			elt = 0;
+			for (int j = 0; j < taille; j++) {
 				// On récupère l'élément de coordonnées (i,j) de la matrice
-				elt = visages.get(i,j);
-				
+				elt = visages.get(i, j);
+
 				// On le soustrait à la moyenne de la ligne
-				elt = elt-this.visage_moyen.get(i,0);
-				
+				elt = elt - this.visage_moyen.get(i, 0);
+
 				// On l'ajoute à la matrice centrée
-				this.matrice_centree.set(i, j,elt);
+				this.matrice_centree.set(i, j, elt);
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println("Classe ACP (TEST)");
-		SimpleMatrix a = new SimpleMatrix(3,2);
-		
+		SimpleMatrix a = new SimpleMatrix(3, 2);
+
 		// Création d'une matrice de test
-		/* ( 3 2 ) 
-		 * ( 5 3 )
-		 * ( 7 5 )
-		 * */
-		double[][] arr = {{3,2},{5,3},{7,5}};
-		
-		for (int i=0;i<3;i++) {
-			
+		/*
+		 * ( 3 2 ) ( 5 3 ) ( 7 5 )
+		 */
+		double[][] arr = { { 3, 2 }, { 5, 3 }, { 7, 5 } };
+
+		for (int i = 0; i < 3; i++) {
+
 			// Matrice 1
-			
+
 			System.out.print("(");
-			for (int j=0;j<2;j++) {
-				System.out.print(" "+arr[i][j]+"");
+			for (int j = 0; j < 2; j++) {
+				System.out.print(" " + arr[i][j] + "");
 				// On copie la matrice 1 en parallèle
 				a.set(i, j, arr[i][j]);
 			}
 			System.out.println(" )");
 		}
-		
+
 		System.out.println(a.toString());
-		
+
 		Acp acp = new Acp(a);
 		// Affichage du visage moyen
 		System.out.println(acp.getVisage_moyen().toString());
--- src/Comparaison.java
+++ src/Comparaison.java
@@ -3,131 +3,136 @@ import org.ejml.simple.SimpleMatrix;
 
 /**
  * Classe Comparaison
+ * 
  * @author virgile
  * @version 0.1
-*/
+ */
 public class Comparaison {
 	/**
-	 * Matrice de l'image à l'origine 
-	 * */
+	 * Matrice de l'image à l'origine
+	 */
 	private SimpleMatrix image;
-	/** 
+	/**
 	 * Matrice déduite de la projection sur l'espace des eigenfaces (la plus proche)
 	 * Pour plus d'informations, se référer à {@link Eigenfaces}
-	 * */
+	 */
 	private SimpleMatrix projete;
-	/** 
+	/**
 	 * Taille de la matrice (chaque matrice image est considérée comme carrée)
-	 * */
+	 */
 	private int taille;
-	
-	/** 
+
+	/**
 	 * Constructeur de la classe
-	 * @param image vectorielle correspondant à l'image
-	 * @param projete matrice prédite
-	 * Modifie {@link Comparaison#image}, {@link Comparaison#projete} et {@link Comparaison#taille}
-	 * */
+	 * 
+	 * @param image   vectorielle correspondant à l'image
+	 * @param projete matrice prédite Modifie {@link Comparaison#image},
+	 *                {@link Comparaison#projete} et {@link Comparaison#taille}
+	 */
 	public Comparaison(SimpleMatrix image, SimpleMatrix projete, int taille) {
 		this.image = image;
 		this.projete = projete;
 		this.taille = taille;
 	}
-	
-	/** 
+
+	/**
 	 * Erreur moyenne quadratique (EQM)
-	 * @return  Erreur moyenne quadratique (EQM)
-	 * Utilise {@link Comparaison#image} et {@link Comparaison#projete}
-	 * */
+	 * 
+	 * @return Erreur moyenne quadratique (EQM) Utilise {@link Comparaison#image} et
+	 *         {@link Comparaison#projete}
+	 */
 	public double calcul_eqm() {
 		double somme = 0;
 		// On réalise la somme de la différence des pixels au carré (formule de l'EQM)
-		for (int i=0;i<taille;i++) {
-			for (int j=0;j<taille;j++) {
-				somme = somme + Math.pow(this.image.get(i,j)-this.projete.get(i,j),2);
+		for (int i = 0; i < taille; i++) {
+			for (int j = 0; j < taille; j++) {
+				somme = somme + Math.pow(this.image.get(i, j) - this.projete.get(i, j), 2);
 			}
 		}
-		return somme/taille;
+		return somme / taille;
 	}
-	
-	/** 
+
+	/**
 	 * Racine de l'erreur moyenne quadratique (REQM)
-	 * @return  Racine de l'erreur moyenne quadratique (REQM)
-	 * Utilise {@link Comparaison#calcul_reqm()}
-	 * */
+	 * 
+	 * @return Racine de l'erreur moyenne quadratique (REQM) Utilise
+	 *         {@link Comparaison#calcul_reqm()}
+	 */
 	public double calcul_reqm() {
 		return Math.sqrt(this.calcul_eqm());
 	}
-	
-	/** 
+
+	/**
 	 * Biais (moyenne des écarts)
-	 * @return biais
-	 * Utilise {@link Comparaison#image} et {@link Comparaison#projete}
-	 * */
+	 * 
+	 * @return biais Utilise {@link Comparaison#image} et
+	 *         {@link Comparaison#projete}
+	 */
 	public double biais() {
 		double somme = 0;
 		// On réalise la somme de la différence des pixels au carré (formule de l'EQM)
-		for (int i=0;i<taille;i++) {
-			for (int j=0;j<taille;j++) {
-				somme = somme + Math.abs(this.image.get(i,j)-this.projete.get(i,j));
+		for (int i = 0; i < taille; i++) {
+			for (int j = 0; j < taille; j++) {
+				somme = somme + Math.abs(this.image.get(i, j) - this.projete.get(i, j));
 			}
 		}
-		return somme/taille;
+		return somme / taille;
 	}
-	
+
 	public static void main(String args[]) {
 		System.out.println("Classe comparaison (TEST)");
-		SimpleMatrix a = new SimpleMatrix(2,2);
-		SimpleMatrix a2 = new SimpleMatrix(2,2);
-		
+		SimpleMatrix a = new SimpleMatrix(2, 2);
+		SimpleMatrix a2 = new SimpleMatrix(2, 2);
+
 		// Création d'une matrice de test
-		/* ( 3 2 ) 
-		 * ( 5 3 )
-		 * */
-		double[][] arr = {{3,2},{5,3}};
+		/*
+		 * ( 3 2 ) ( 5 3 )
+		 */
+		double[][] arr = { { 3, 2 }, { 5, 3 } };
 		// Création d'une deuxième matrice de test
-		/* ( 1 5 )
-		 * ( 7 6 )
-		 * */
-		double[][] arr2 = {{1,5},{7,6}};
-		
+		/*
+		 * ( 1 5 ) ( 7 6 )
+		 */
+		double[][] arr2 = { { 1, 5 }, { 7, 6 } };
+
 		// Description des matrices
 		System.out.println("Matrice gauche : matrice d'origine");
 		System.out.println("Matrice droite : matrice des eigenfaces la plus proche");
 		// Affichage des matrices en parallèle
-		for (int i=0;i<2;i++) {
-			
+		for (int i = 0; i < 2; i++) {
+
 			// Matrice 1
-			
+
 			System.out.print("(");
-			for (int j=0;j<2;j++) {
-				System.out.print(" "+arr[i][j]+"");
+			for (int j = 0; j < 2; j++) {
+				System.out.print(" " + arr[i][j] + "");
 				// On copie la matrice 1 en parallèle
 				a.set(i, j, arr[i][j]);
 			}
-			
+
 			System.out.print(" )");
-			
+
 			System.out.print(" (");
 			// Matrice 2
-			for (int j=0;j<2;j++) {
-				System.out.print(" "+arr2[i][j]+"");
+			for (int j = 0; j < 2; j++) {
+				System.out.print(" " + arr2[i][j] + "");
 				// On copie la matrice en parallèle
 				a2.set(i, j, arr2[i][j]);
 			}
-			
+
 			System.out.println(" )");
-			
+
 		}
-		
-		//On crée la Comparaison et on teste la classe
-		
-		Comparaison c = new Comparaison(a,a2,2);
-		
+
+		// On crée la Comparaison et on teste la classe
+
+		Comparaison c = new Comparaison(a, a2, 2);
+
 		// EQM
-		System.out.println("EQM : "+c.calcul_eqm());
+		System.out.println("EQM : " + c.calcul_eqm());
 		// REQM
-		System.out.println("REQM : "+c.calcul_reqm());
+		System.out.println("REQM : " + c.calcul_reqm());
 		// Biais
-		System.out.println("Biais : "+c.biais());
+		System.out.println("Biais : " + c.biais());
 	}
 }
